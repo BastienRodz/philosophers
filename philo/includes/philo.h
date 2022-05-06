4721cdff131af5f42a15fc0dde7964a6315a9498
Bastien RODRIGUEZ <barodrig@e3r5p20.clusters.42paris.fr>
2022-05-06 18:25:35 +0200

    Norm ok

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: barodrig <barodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 16:03:43 by barodrig          #+#    #+#             */
-/*   Updated: 2022/05/06 14:36:13 by barodrig         ###   ########.fr       */
+/*   Updated: 2022/05/06 18:15:46 by barodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,21 @@
 # include <sys/time.h>
 # include <time.h>
 
-struct s_philo;
-struct s_data;
+struct	s_philo;
+struct	s_data;
 
 // Struct that carry all the datas about one philo;
 typedef struct s_philo
 {
-	int     			id;
+	int					id;
 	int					meal_nbr;
 	long int			tm_last_meal;
 	int					left;
 	int					right;
 	pthread_mutex_t		*meal_lock;
-	pthread_t 			*monitor;
+	pthread_t			*monitor;
 	struct s_data		*data;
-}               		t_philo;
+}						t_philo;
 
 //Struct that carry all the args every philo needs;
 typedef struct s_data
@@ -46,15 +46,14 @@ typedef struct s_data
 	long int			tm_need_eat;
 	long int			one_dead;
 	long int			tm_start;
-	long int			tot_meals;  
+	long int			tot_meals;
 	t_philo				*philos;
 	pthread_t			*philo_th;
 	pthread_mutex_t		*mutex_fork;
 	pthread_mutex_t		*mutex_print;
 	pthread_mutex_t		*mutex_dead;
 	pthread_mutex_t		*mutex_meal;
-}              			t_data;
-
+}						t_data;
 
 /* ROUTINE MONITOR */
 void		*monitor_routine(void *p_data);
@@ -63,7 +62,7 @@ void		*monitor_routine(void *p_data);
 void		*philo_routine(void *p_data);
 
 /* PRINT */
-int 		ft_putstr_fd(char *str, int fd);
+int			ft_putstr_fd(char *str, int fd);
 void		printer(t_philo *philo, int id, char *str);
 
 /* TIME */
